b172354809e593093a07c07767587655f304479c
Jaystarz360 <Joshua.Cherry1985@gmail.com>
2024-01-08 02:26:00 -0500

    Multiple Changes

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generatebottonBackground = document.querySelector("#generate");
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generatebottonBackground.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
